5b4b042122b7d1c90af9eac106623210d9dbcf5b
Marcos Gabriel <56852736+Coronel-Marc@users.noreply.github.com>
2021-08-21 22:13:10 -0300

    Initial commit

---
+++ src/ImportApp/Principal.java
@@ -0,0 +1,229 @@
+package ImportApp;
+import java.util.Scanner;
+
+
+/*
+* Necessário implentação de um método ou recursos para limpar a tela sempre que atualizar
+*
+* */
+public class Principal {
+
+    private final Produto[] produtosList = new Produto[10];
+    protected int atualPosicao = 0;
+
+
+
+    public static void main(String[] args){
+        Principal app = new Principal();
+        titulo();
+        app.menuPrincipal();
+
+    }
+
+
+    private void menuPrincipal(){
+        int opcaoUsuario = 0;
+
+        do{
+            opcaoUsuario = opcaoMenu();
+            switch (opcaoUsuario) {
+                case 1:
+                    menuCadastroProduto();
+                    break;
+
+                case 2:
+                    menuMovimentacao();
+                    break;
+
+                case 3:
+                    menuReajustePreco();
+                    break;
+
+                case 4:
+                    relatorios();
+                    break;
+
+                case 0:
+//                    finalizar();
+                    System.out.println("Finalizando o programa. Até a proxima.");
+                    break;
+
+                default:
+                    System.out.println("Opção inválida");
+                    break;
+            }
+            //Finaliza o Scanner
+        } while (opcaoUsuario != 0);
+
+
+
+
+    }
+
+
+    //INICIO METODO PARA APRESENTAR MENU ----------------------
+    private static void titulo(){
+        System.out.println("IMPORTAPP SOLUTIONS LTDA\nSISTEMA DE CONTROLE DE ESTOQUE\nOPÇÃO: ");
+    }
+    private int opcaoMenu() {
+        int opcao;
+        System.out.println("MENU PRINCIPAL\n" +
+                "1 - CADASTRO DE PRODUTOS\n" +
+                "2 - MOVIMENTAÇÃO\n" +
+                "3 - REAJUSTE DE PREÇO\n" +
+                "4 - RELATÓRIOS\n" +
+                "0 - FINALIZAR\n" +
+                "OPÇÃO  :");
+        opcao = getEscolhaMenu();
+        return opcao;
+    }
+    private int getEscolhaMenu() {
+        Scanner scanner = new Scanner(System.in);
+        return Integer.parseInt(scanner.next());
+    }
+    //FIM METODO PARA APRESENTAR MENU
+
+    //----------------------
+
+    //INICIO METODOS DO MENU PRINCIPAL
+
+    private void relatorios(){
+        titulo();
+        System.out.println("RELATÓRIO");
+
+        for (int i = 0; i < atualPosicao; i++){
+            System.out.println("\n//---------------------//: \n" +
+                    "CÓDIGO: " + i + "\n" +
+                    produtosList[i]);
+        }
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\n\n\n");
+        System.out.println("APERTE QUALQUER LETRA + ENTER PARA CONTINUAR");
+        scanner.next();
+
+    }
+
+    private void menuCadastroProduto(){
+        int opcao;
+        System.out.println("" +
+                "1 - INCLUSÃO\n" +
+                "2 - ALTERAÇÃO\n" +
+                "3 - CONSULTA\n" +
+                "4 - EXCLUSÃO\n" +
+                "0 - RETORNAR\n");
+        opcao = getEscolhaMenu();
+
+        switch (opcao) {
+            case 1:
+                cadastrarProduto();
+                break;
+            case 2:
+                alterarProduto();
+                break;
+            case 3:
+                consultarProduto();
+                break;
+            case 4:
+                excluirProduto();
+                break;
+            default:
+                System.out.println("//--------------------------------//\nRetornando ao menu");
+                break;
+        }
+    }
+
+    private void menuMovimentacao(){
+        System.out.println("teste2");
+
+    }
+
+    private void menuReajustePreco(){
+        System.out.println("teste3");
+
+    }
+    //FIM METODOS DO MENU PRINCIPAL
+
+    //--------------------------
+
+    //INICIO METODOS MENU DE CADASTRO
+    private void cadastrarProduto(){
+        String escolha = "";
+        do{
+            titulo();
+            System.out.println("CADASTRAR PRODUTO");
+            Produto produto = setDadosProduto();
+            escolha = confirmaOperacao();
+            if(escolha.equalsIgnoreCase("S")){
+                produtosList[atualPosicao] = produto;
+                atualPosicao++;
+            }
+            escolha = getRepetirOperacao();
+
+        }while (escolha.equalsIgnoreCase("S"));
+    }
+
+    private void alterarProduto(){}
+
+    private void consultarProduto(){}
+
+    private void excluirProduto(){}
+
+
+
+    //------------------------------------------------------
+
+    //FIM METODOS MENU DE CADASTRO
+
+    //------------------------------------------------------
+
+    //Confirmação de operação
+
+    private String confirmaOperacao(){
+        Scanner scanner = new Scanner(System.in);
+        String escolha;
+        System.out.println("CONFIRMA OPERAÇÃO? (S/N): ");
+        escolha =  scanner.next();
+        return escolha;
+    }
+
+    private String getRepetirOperacao(){
+        Scanner scanner = new Scanner(System.in);
+        String escolha;
+        System.out.println("REPETIR OPERAÇÃO? (S/N): ");
+        escolha = scanner.next();
+        return escolha;
+    }
+
+    //Fim confirmação de operação
+
+    //------------------------------------------------------
+    private Produto setDadosProduto(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Insira o nome do produto: ");
+        String nome = scanner.nextLine();
+
+        System.out.println("Insira o preço unitário do produto: ");
+        float precoUnitario = scanner.nextInt();
+
+        System.out.println("Insira a quantidade de unidades a serem adicionadas: ");
+        int unidade = scanner.nextInt();
+
+        int id = atualPosicao;
+
+        Produto produto = new Produto(nome, precoUnitario, unidade,id);
+
+        produto.setNome(nome);
+        produto.setPrecoUnitario(precoUnitario);
+        produto.setUnidade(unidade);
+        produto.setId(id);
+        return produto;
+
+
+    }
+}
+
+
+
+
+
---
+++ src/ImportApp/Produto.java
@@ -0,0 +1,56 @@
+package ImportApp;
+
+public class Produto {
+    private int id;
+    private String nome;
+    private float precoUnitario;
+    private int unidade;
+
+
+
+
+    public String getNome(){    return nome;    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public float getPrecoUnitario(){    return precoUnitario;   }
+    public void setPrecoUnitario(float precoUnitario){
+        this.precoUnitario = precoUnitario;
+    }
+
+    public int getUnidade(){    return unidade;  }
+    public void setUnidade(int unidade){
+        this.unidade = unidade;
+    }
+
+    public int getId(){      return id;    }
+    public void setId(int id){
+        this.id = id;
+    }
+
+    //Construtor padrão
+//    public Produto(String nome, float precoUnitario, int unidade) {
+//
+//    }
+
+    //Construtor do produto
+    public Produto(String nome, float precoUnitario, int unidade, int id){
+        nome = this.nome;
+        precoUnitario = this.precoUnitario;
+        unidade = this.unidade;
+        id = this.id;
+
+    }
+
+    @Override
+    public String toString(){
+        return "NOME: " + nome + "\n"
+                +"PREÇO UNITÁRIO: R$" + precoUnitario + "\n"
+                +"QUANTIDADE EM ESTOQUE: " + unidade + "\n";
+    }
+
+
+
+
+}
